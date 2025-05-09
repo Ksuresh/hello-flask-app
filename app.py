575397adad31c8dff92e24a60271d30fae8096d2
Suresh <suresh.kandasamy@sumerusolutions.com>
2025-05-09 17:17:17 +0530

    Initial commit - working Flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Suresh! DevSecOps Demo is Working 🎉"
+
+if __name__ == '__main__':
+    app.run(debug=True)
